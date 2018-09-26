4d800e7be79dd6f7158d3ae82becada4cad00987
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-09-26 20:52:09 +0200

    replace copy url with our SF links
    *nuke lineage guide link from INFO dialog box

--- src/org/lineageos/updater/UpdatesListAdapter.java
+++ src/org/lineageos/updater/UpdatesListAdapter.java
@@ -22,6 +22,7 @@ import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.BatteryManager;
 import android.os.PowerManager;
+import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 import android.text.SpannableString;
 import android.text.format.Formatter;
@@ -504,7 +505,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
                 case R.id.menu_copy_url:
                     Utils.addToClipboard(mActivity,
                             mActivity.getString(R.string.label_download_url),
-                            update.getDownloadUrl(),
+                            SFUrl(update),
                             mActivity.getString(R.string.toast_download_url_copied));
                     return true;
                 case R.id.menu_export_update:
@@ -523,6 +524,16 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
         helper.show();
     }
 
+    private String SFUrl(UpdateInfo update) {
+	String UpVersion = "arrow-" + update.getVersion().substring(1,3) + "x";
+
+	String sfdownurl = mActivity.getString(R.string.sf_download_url);
+
+	return sfdownurl.replace("{device}", SystemProperties.get(Constants.PROP_DEVICE))
+                        .replace("{version}", UpVersion)
+                        .replace("{zip}", update.getName()).trim();
+    }
+
     private void exportUpdate(UpdateInfo update) {
         File dest = new File(Utils.getExportPath(mActivity), update.getName());
         if (dest.exists()) {
@@ -537,10 +548,8 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
 
     private void showInfoDialog() {
         String messageString = String.format(StringGenerator.getCurrentLocale(mActivity),
-                mActivity.getString(R.string.blocked_update_dialog_message),
-                mActivity.getString(R.string.blocked_update_info_url));
+                mActivity.getString(R.string.blocked_update_dialog_message));
         SpannableString message = new SpannableString(messageString);
-        Linkify.addLinks(message, Linkify.WEB_URLS);
         AlertDialog dialog = new AlertDialog.Builder(mActivity)
                 .setTitle(R.string.blocked_update_dialog_title)
                 .setPositiveButton(android.R.string.ok, null)
