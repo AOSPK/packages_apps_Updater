36e7d44eb48e3b46916b2773eb78cbc8b8907976
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-09-28 08:45:52 +0200

    Revert "Updater: Make upgrade-url device specific"
    This reverts commit 5763bc9d388b7d7d1be04cb64951dfa6b1a07643.

--- src/org/lineageos/updater/UpdatesListAdapter.java
+++ src/org/lineageos/updater/UpdatesListAdapter.java
@@ -538,7 +538,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
     private void showInfoDialog() {
         String messageString = String.format(StringGenerator.getCurrentLocale(mActivity),
                 mActivity.getString(R.string.blocked_update_dialog_message),
-                Utils.getUpgradeBlockedURL(mActivity));
+                mActivity.getString(R.string.blocked_update_info_url));
         SpannableString message = new SpannableString(messageString);
         Linkify.addLinks(message, Linkify.WEB_URLS);
         AlertDialog dialog = new AlertDialog.Builder(mActivity)
