40c35850388043b441b0138368dddd033296aec7
Alexandre Rangel <mamutal91@gmail.com>
2021-02-18 16:41:55 -0300

    Updater: Update the props
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>
    Change-Id: I54b9b5e9d62fe71c5a7baeb42c14fdc457eca523

--- src/org/kraken/updater/misc/Constants.java
+++ src/org/kraken/updater/misc/Constants.java
@@ -43,8 +43,8 @@ public final class Constants {
     public static final String PROP_DEVICE = "ro.custom.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
     public static final String PROP_RELEASE_TYPE = "ro.custom.apps.version";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
-    public static final String PROP_UPDATER_URI = "lineage.updater.uri";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "custom.updater.allow_downgrading";
+    public static final String PROP_UPDATER_URI = "custom.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
